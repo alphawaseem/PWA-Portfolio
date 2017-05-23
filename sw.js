127d70a11db7a73361788af23da55ff0099ebac4
Waseem Ahmed <alphawaseem@gmail.com>
2017-05-23 11:32:18 +0530

    cache static files

--- sw.js
+++ sw.js
@@ -1,9 +1,21 @@
 (function() {
     'use strict';
 
+    var CACHE_NAME = 'static-cache';
+
+    var urlsToCache = [
+        '.',
+        'index.html',
+        'styles/main.css'
+    ];
     // TODO - 3.1: Add install and activate event listeners
-    self.addEventListener('install', (event) => {
-        console.log('New Service working installing');
+    self.addEventListener('install', function(event) {
+        event.waitUntil(
+            caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+        );
     });
 
     self.addEventListener('activate', (event) => {
@@ -13,7 +25,31 @@
     // My new comment
 
     self.addEventListener('fetch', function(event) {
-        console.log('Fetching:', event.request.url);
+        event.respondWith(
+            caches.match(event.request)
+            .then(function(response) {
+                return response || fetchAndCache(event.request);
+            })
+        );
     });
+
+    function fetchAndCache(url) {
+        return fetch(url)
+            .then(function(response) {
+                // Check if we received a valid response
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                return caches.open(CACHE_NAME)
+                    .then(function(cache) {
+                        cache.put(url, response.clone());
+                        return response;
+                    });
+            })
+            .catch(function(error) {
+                console.log('Request failed:', error);
+                // You could return a custom offline 404 page here
+            });
+    }
 
 })();
